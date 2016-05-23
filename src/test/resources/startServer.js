9e1c7ed7c90187c4d52c0c8eccee3ce5dd574a3f
Joshua Marquez <joshua.marquez.n@gmail.com>
2016-05-22 20:54:20 -0700

    /count route added to Sails Server

--- src/test/resources/startServer.js
+++ src/test/resources/startServer.js
@@ -5,6 +5,16 @@ sailsServer.setup(function(err) {
         throw err;
     }
 
+    server.router.bind("/count", function (req, res) {
+        var count;
+        if (!req.session) {
+            return res.send("NO_SESSION");
+        }
+        count = req.session.count || 1;
+        req.session.count = count + 1;
+        return res.send(200, count);
+    });
+
     console.log('Sails server running.');
 });
 
