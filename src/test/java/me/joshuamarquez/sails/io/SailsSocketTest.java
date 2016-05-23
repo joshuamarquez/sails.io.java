06bcde2a1f0948ffbfa955cdbaf68db7ada82bc4
Joshua Marquez <joshua.marquez.n@gmail.com>
2016-05-22 20:56:57 -0700

    headers and session socket tests added

--- src/test/java/me/joshuamarquez/sails/io/SailsSocketTest.java
+++ src/test/java/me/joshuamarquez/sails/io/SailsSocketTest.java
@@ -1,10 +1,11 @@
 package me.joshuamarquez.sails.io;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 import me.joshuamarquez.sails.io.util.SIJUtils;
-import org.hamcrest.CoreMatchers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
@@ -13,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -26,6 +28,13 @@ public class SailsSocketTest extends SailsServer {
     private final String TAG = "SailsSocketTest";
 
     private JSONObject EXPECTED_RESPONSES;
+
+    private Map<String, String> headers = new HashMap<String, String>(){
+        {
+            put("x-test-header-one", "foo");
+            put("x-test-header-two", "bar");
+        }
+    };
 
     @Before
     public void setUp() throws Exception {
@@ -50,7 +59,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void shouldReturnStatusCodeNotFound() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -70,7 +79,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testGet() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -82,7 +91,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testDelete() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -94,7 +103,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testPost() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -106,7 +115,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testPut() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -118,7 +127,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testGetJSON() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -130,7 +139,7 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.disconnect();
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void testGetError() throws Exception {
         final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
 
@@ -140,6 +149,126 @@ public class SailsSocketTest extends SailsServer {
         sailsSocket.connect();
         values.take();
         sailsSocket.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetHeaders() throws Exception {
+        final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
+
+        SailsSocket sailsSocket = new SailsSocket(url, new IO.Options());
+        sailsSocket.setHeaders(headers);
+
+        sailsSocket.get(TAG, "/headers", null, buildResponseListener("get /headers", values));
+
+        sailsSocket.connect();
+        values.take();
+        sailsSocket.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetHeadersOverride()throws Exception {
+        final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
+
+        SailsIOClient.getInstance().setHeaders(headers);
+        SailsSocket sailsSocket = SailsIOClient.getInstance().socket(url, new IO.Options());
+
+        sailsSocket.request(TAG, SailsSocketRequest.METHOD_GET, "/headersOverride", null,
+                new HashMap<String, String>(){
+                    {
+                        put("x-test-header-one", "baz");
+                    }
+                }, buildResponseListener("get /headersOverride", values));
+
+        sailsSocket.connect();
+        values.take();
+        sailsSocket.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testGetHeadersRemove()throws Exception {
+        final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
+
+        SailsIOClient.getInstance().setHeaders(headers);
+        SailsSocket sailsSocket = SailsIOClient.getInstance().socket(url, new IO.Options());
+
+        sailsSocket.request(TAG, SailsSocketRequest.METHOD_GET, "/headersRemove", null,
+                new HashMap<String, String>(){
+                    {
+                        put("x-test-header-one", null);
+                    }
+                }, buildResponseListener("get /headersRemove", values));
+
+        sailsSocket.connect();
+        values.take();
+        sailsSocket.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testOneSocketSession() throws Exception {
+        final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
+
+        SailsSocket sailsSocket = new SailsSocket(url, new IO.Options());
+
+        sailsSocket.get(TAG, "/count", null, new SailsSocketResponse.Listener() {
+            @Override
+            public void onResponse(JWR response) {
+                assertEquals(Integer.parseInt(response.getBody().toString()), 1);
+
+                sailsSocket.get(TAG, "/count", null, new SailsSocketResponse.Listener() {
+                    @Override
+                    public void onResponse(JWR response) {
+                        assertEquals(Integer.parseInt(response.getBody().toString()), 2);
+
+                        values.offer("done");
+                    }
+                });
+            }
+        });
+
+        sailsSocket.connect();
+        values.take();
+        sailsSocket.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testTwoSocketsNotSharingSession() throws Exception {
+        final BlockingQueue<Object> values = new LinkedBlockingQueue<Object>();
+
+        SailsSocket sailsSocket1 = new SailsSocket(url, new IO.Options());
+        SailsSocket sailsSocket2 = new SailsSocket(url, new IO.Options());
+
+        sailsSocket1.get(TAG, "/count", null, new SailsSocketResponse.Listener() {
+            @Override
+            public void onResponse(JWR response) {
+                assertEquals(Integer.parseInt(response.getBody().toString()), 1);
+
+                sailsSocket2.get(TAG, "/count", null, new SailsSocketResponse.Listener() {
+                    @Override
+                    public void onResponse(JWR response) {
+                        assertEquals(Integer.parseInt(response.getBody().toString()), 1);
+
+                        values.offer("done");
+                    }
+                });
+                sailsSocket2.connect();
+            }
+        });
+
+        sailsSocket1.connect();
+        values.take();
+        sailsSocket1.disconnect();
+        sailsSocket2.disconnect();
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void shouldGetCookieFromSetCookieHeader() throws Exception {
+        // Make a request to Sails' built-in /__getcookie route
+        HttpResponse<String>stringResponse = Unirest.get("http://localhost:1577/__getcookie").asString();
+        String setCookieHeader = stringResponse.getHeaders().get("set-cookie").toString();
+        // Get the cookie data from the set-cookie header
+        String cookie = setCookieHeader.substring(setCookieHeader.indexOf("[") + 1, setCookieHeader.indexOf(";"));
+
+        assertNotNull(cookie);
     }
 
     /**
@@ -165,8 +294,6 @@ public class SailsSocketTest extends SailsServer {
     private void assertResponse(String routeAddress, JWR response) {
         // Ensure JWR JSON Object is not null
         assertNotNull(response.getJsonObjectResponse());
-
-        System.out.println(response.getJsonObjectResponse().toString());
 
         // Ensure body is the correct value
         Object expectedBody;
